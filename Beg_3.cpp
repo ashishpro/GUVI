695faff5d2339a330470f5fed5d8ee0da6683c9e
Ashish Kumar <ashishpro@users.noreply.github.com>
2017-07-01 19:41:07 +0530

    Create Beg_3.cpp

---
+++ Beg_3.cpp
@@ -0,0 +1,14 @@
+#include<string>
+#include <iostream>
+
+
+int main()
+{
+	char str;
+	std::cin >> str;std::cin.ignore();
+	str = toupper(str);
+	std::cout << str;
+	std::string ans = (str == 'A' || str == 'E' || str == 'I' || str == 'O' || str == 'U')?("VOWEL"):("CONSONANT");
+	std::cout << ans;
+	return 0;
+}
