96a789bceafdfb5f125a06564155fb09a585bdc5
Ashish Kumar <ashishpro@users.noreply.github.com>
2017-07-02 01:17:58 +0530

    Update Beg_10.cpp

--- Beg_10.cpp
+++ Beg_10.cpp
@@ -6,19 +6,24 @@ int main()
 {
 	int year(0);
 	std::cin >> year;
-	if (year % 4 == 0)
+	if (year > 0)
 	{
-		if (year % 100 == 0)
+		if (year % 4 == 0)
 		{
-			if (year % 400 == 0)
-				std::cout << "Leap Year";
+			if (year % 100 == 0)
+			{
+				if (year % 400 == 0)
+					std::cout << "Leap Year";
+				else
+					std::cout << "Not A Leap Year";
+			}
 			else
-				std::cout << "Not A Leap Year";
+				std::cout << "Leap Year";
 		}
 		else
-			std::cout << "Leap Year";
+			std::cout << "Not A Leap Year";
 	}
 	else
-		std::cout << "Not A Leap Year";
+		std::cout << "Invalid Year";
 	return 0;
 }
