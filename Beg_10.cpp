364d829b8e367acd91442d3b90142d9c34523392
Ashish Kumar <ashishpro@users.noreply.github.com>
2017-07-02 01:15:02 +0530

    Create Beg_10.cpp

---
+++ Beg_10.cpp
@@ -0,0 +1,24 @@
+
+#include<string>
+#include <iostream>
+
+int main()
+{
+	int year(0);
+	std::cin >> year;
+	if (year % 4 == 0)
+	{
+		if (year % 100 == 0)
+		{
+			if (year % 400 == 0)
+				std::cout << "Leap Year";
+			else
+				std::cout << "Not A Leap Year";
+		}
+		else
+			std::cout << "Leap Year";
+	}
+	else
+		std::cout << "Not A Leap Year";
+	return 0;
+}
