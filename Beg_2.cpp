d9b0f56509851730702394ccdf27fcba57d4f176
Ashish Kumar <ashishpro@users.noreply.github.com>
2017-07-01 19:27:46 +0530

    Create Beg_2.cpp

---
+++ Beg_2.cpp
@@ -0,0 +1,13 @@
+
+#include<string>
+#include <iostream>
+
+
+int main()
+{
+	int number = 0;
+	std::cin >> number;std::cin.ignore();
+	std::string ans = (number % 2 != 0) ? ("ODD") : ("EVEN");
+	std::cout << ans;
+	return 0;
+}
